dbf292a2bb368faa964701a40a4de23e6cb8fd2b
Zach Haedike <haedikez@ufl.edu>
2025-11-12 09:11:10 -0500

    2nd calc change

--- calculator.py
+++ calculator.py
@@ -5,11 +5,24 @@ calculator.py
 One function per operation, in order.
 """
 import math
-def add(a, b): a + b
 
-def sub(a, b): a - b
+def square_root(a):
+    try:
+        return math.sqrt(a)
+    except ValueError:
+        print("Number cannot be negative") # raise ValueError if a < 0
 
-def mul(a, b): a * b
+def hypotenuse(a, b):
+    return math.hypot(a, b) # can have negative nums
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
 
 def div(a, b):
     if a == 0:
@@ -23,4 +36,5 @@ def log(a, b):
     else:
         math.log(b, a) # use math library + raise ValueError
 
-def exp(a, b): a ** b
+def exp(a, b):
+    return a ** b
